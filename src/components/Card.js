aef89dac747cd47323a4df62f668430365939134
charliedodds <charlesgdodds@hotmail.co.uk>
2021-02-23 12:02:42 +0000

    Change links to match routes

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,29 +7,29 @@ import Rating from './Rating';
 import '../styles/Card.css';
 
 const Card = ({ item }) => {
-  const imgSrc = editImgSrc(item.thumb);
+	const imgSrc = editImgSrc(item.thumb);
 
-  return (
-    <Link to={`/shop/${item.dealID}`} className='Card-link'>
-      <article className='Card'>
-        <figure className='Card-img-container'>
-          <img
-            className='Card-img'
-            src={imgSrc}
-            alt={`${item.title} thumbnail`}
-          />
-        </figure>
-        <h2 className='Card-title'>{item.title}</h2>
-        <section className='Card-info'>
-          <p className='Card-price'>£{item.salePrice}</p>
-          <Rating
-            count={item.steamRatingCount}
-            rating={item.steamRatingPercent}
-          />
-        </section>
-      </article>
-    </Link>
-  );
+	return (
+		<Link to={`/shopping-cart/shop/${item.dealID}`} className='Card-link'>
+			<article className='Card'>
+				<figure className='Card-img-container'>
+					<img
+						className='Card-img'
+						src={imgSrc}
+						alt={`${item.title} thumbnail`}
+					/>
+				</figure>
+				<h2 className='Card-title'>{item.title}</h2>
+				<section className='Card-info'>
+					<p className='Card-price'>£{item.salePrice}</p>
+					<Rating
+						count={item.steamRatingCount}
+						rating={item.steamRatingPercent}
+					/>
+				</section>
+			</article>
+		</Link>
+	);
 };
 
 export default Card;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,41 +3,45 @@ import { NavLink } from 'react-router-dom';
 import '../styles/Navbar.css';
 
 const Navbar = ({ numItems }) => {
-  return (
-    <nav className='Navbar'>
-      <figure className='Navbar-logo'>
-        <i className='fas fa-gamepad'></i>
-      </figure>
-      <ul className='Navbar-links'>
-        <li className='Navbar-li'>
-          <NavLink
-            exact
-            to='/'
-            className='Navbar-link'
-            activeClassName='Navbar-active'
-          >
-            Home
-          </NavLink>
-        </li>
-        <li className='Navbar-li'>
-          <NavLink
-            exact
-            to='/shop'
-            className='Navbar-link'
-            activeClassName='Navbar-active'
-          >
-            Shop
-          </NavLink>
-        </li>
-      </ul>
-      <NavLink exact to='/checkout' activeClassName='Navbar-active'>
-        <button className='Navbar-btn'>
-          <i className='fas fa-shopping-cart'></i>
-          <span className='Navbar-num-items'>{numItems}</span>
-        </button>
-      </NavLink>
-    </nav>
-  );
+	return (
+		<nav className='Navbar'>
+			<figure className='Navbar-logo'>
+				<i className='fas fa-gamepad'></i>
+			</figure>
+			<ul className='Navbar-links'>
+				<li className='Navbar-li'>
+					<NavLink
+						exact
+						to='/shopping-cart/'
+						className='Navbar-link'
+						activeClassName='Navbar-active'
+					>
+						Home
+					</NavLink>
+				</li>
+				<li className='Navbar-li'>
+					<NavLink
+						exact
+						to='/shopping-cart/shop'
+						className='Navbar-link'
+						activeClassName='Navbar-active'
+					>
+						Shop
+					</NavLink>
+				</li>
+			</ul>
+			<NavLink
+				exact
+				to='/shopping-cart/checkout'
+				activeClassName='Navbar-active'
+			>
+				<button className='Navbar-btn'>
+					<i className='fas fa-shopping-cart'></i>
+					<span className='Navbar-num-items'>{numItems}</span>
+				</button>
+			</NavLink>
+		</nav>
+	);
 };
 
 export default Navbar;
